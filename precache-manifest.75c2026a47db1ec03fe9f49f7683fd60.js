5ceb516b8c738f8bc5bb5d7eac87a24f9b2c5f24
Danny <danielhamam@hotmail.com>
2021-05-03 18:49:33 -0400

    Updates

---
+++ precache-manifest.75c2026a47db1ec03fe9f49f7683fd60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2402700d7cbf89dcb31a5bc0580167e",
+    "url": "/TodoList-React-Redux/index.html"
+  },
+  {
+    "revision": "49d2c44e4f4f88eb07ff",
+    "url": "/TodoList-React-Redux/static/css/main.314423be.chunk.css"
+  },
+  {
+    "revision": "36efbda4356cb32cd888",
+    "url": "/TodoList-React-Redux/static/js/2.fb99ad80.chunk.js"
+  },
+  {
+    "revision": "49d2c44e4f4f88eb07ff",
+    "url": "/TodoList-React-Redux/static/js/main.9483dc9f.chunk.js"
+  },
+  {
+    "revision": "25fddbb38585f6063b37",
+    "url": "/TodoList-React-Redux/static/js/runtime-main.c6b7774b.js"
+  }
+]);
